5ffbef96f46c51cc8a46c2aa22a09c31a25bd6e5
Ksenx73 <hello@itcampe.com>
2021-10-19 12:57:46 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,18 @@
+package it.campe;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+        int multiple = scanner.nextInt();
+
+        int sum = 0;
+        //напишите тут ваш код
+
+        System.out.println(sum);
+    }
+}
